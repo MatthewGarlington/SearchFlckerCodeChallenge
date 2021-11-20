783a1ca61df5f24d346280b8eaf04efee0de55f1
Matthew Garlington <sbmatthewg@gmail.com>
2021-11-20 10:51:58 -0500

    Initial Commit

---
+++ AetnaCodeChallenge/AetnaCodeChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AetnaCodeChallengeApp.swift
+//  AetnaCodeChallenge
+//
+//  Created by us-guest on 11/20/21.
+//
+
+import SwiftUI
+
+@main
+struct AetnaCodeChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
